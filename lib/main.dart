eb410be44e8c9864dbd2e59558fd331e14ad50fd
Raven Udani <udaniraven@gmail.com>
2024-10-24 01:57:18 +0800

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,88 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.black,
         appBar: AppBar(
-          title: const Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: const Text('Dice'),
+          backgroundColor: Colors.white,
         ),
-        body: const DicePage(),
+        body: DicePage(),
       ),
     ),
   );
 }
-
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int left = 1;
+  int right = 1;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        children: [
+          Expanded(child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+                onPressed: (){
+                  setState(() {
+                    left = 1 + Random().nextInt(6);
+                    right = 1 + Random().nextInt(6);
+                  });
+
+                  print('Left image is clicked');
+                  print(left);
+                },
+                child: Image.asset('images/dice$left.png')),
+          )),
+          Expanded(child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: TextButton(
+                onPressed: (){
+                  setState(() {
+                    left = 1 + Random().nextInt(6);
+                    right = 1 + Random().nextInt(6);
+                  });
+                  print('Right image is clicked');
+                },
+                child: Image.asset('images/dice$right.png')),
+          )),
+          const SizedBox(
+            height: 20,
+          ),
+          TextButton(
+            onPressed: (){
+              setState(() {
+                left = 1;
+                right = 1;
+              });
+            },
+            child: const Text(
+              'Reset',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+              ),
+            )
+          ),
+        ],
+      ),
+
+    );
+
   }
 }
+
+
+
